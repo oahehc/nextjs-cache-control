5cd1cd8ed3e6701105064b9e2ccb375873c819bf
oahehc <oahehc@gmail.com>
2020-05-03 21:40:38 +0800

    update setting

--- pages/product.js
+++ pages/product.js
@@ -21,9 +21,11 @@ export default Product;
 
 export const getServerSideProps = async ({ res }) => {
   if (res) {
+    // res.setHeader("age", 2);
+    // res.setHeader("date", "Sun, 03 May 2020 12:32:41 GMT");
     res.setHeader(
       "Cache-Control",
-      "public, max-age=10, stale-while-revalidate=20"
+      "public,max-age=10,stale-while-revalidate=20"
     );
     res.setHeader("ETag", generateETag(30));
   }
